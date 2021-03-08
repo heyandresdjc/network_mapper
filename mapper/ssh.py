c795d7899c72e352c09fc63a7bd61aefa774edc6
Andres DJC <acabrera@adfone.com>
2021-03-08 13:21:50 -0500

    better error handling in SSH client

--- mapper/ssh.py
+++ mapper/ssh.py
@@ -4,19 +4,18 @@ from main import map_network
 for ip in map_network():
     print("Checking... ", ip)
     try:
-        try:
-            server = SSHTunnelForwarder(
-                ip,
-                ssh_username="pi",
-                remote_bind_address=('127.0.0.1', 8080)
-            )
-            server.start()
-            print(server.local_bind_port)  # show assigned local port
-            # work with `SECRET SERVICE` through `server.local_bind_port`.
-            server.stop()
-        except BaseSSHTunnelForwarderError:
-            pass
-        except HandlerSSHTunnelForwarderError:
-            print("Failed... ", ip)
+        server = SSHTunnelForwarder(
+            ip,
+            ssh_username="pi",
+            remote_bind_address=('127.0.0.1', 8080)
+        )
+        server.start()
+        print(server.local_bind_port)  # show assigned local port
+        # work with `SECRET SERVICE` through `server.local_bind_port`.
+        server.stop()
+    except BaseSSHTunnelForwarderError:
+        print("Couldn't connect to...", ip)
+    except HandlerSSHTunnelForwarderError:
+        print("Failed... ", ip)
     except Exception as ex:
         print(ex.__str__())
